f1cb1fb8eafa5bdf3b7345b1c36808aaf6beecec
horaceheaven <hheaven@medullan.com>
2015-09-19 20:20:03 -0400

    created layout footer

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var vm = { 
+		title: 'Express',  
+		currentYear: new Date().getFullYear() 
+	};
+	
+  	res.render('index', vm);
 });
 
 module.exports = router;
