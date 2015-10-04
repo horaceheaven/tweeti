ad76726c9e9ea31eaf438421fbd17380e4733936
horaceheaven <hheaven@medullan.com>
2015-10-04 13:54:01 -0400

    created is auth endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,19 @@ var passport = require('passport');
 var router = express.Router();
 
 router.get('/twitter', passport.authenticate('twitter'));
+
 router.get('/twitter/callback', passport.authenticate('twitter', { 
 	failureRedirect: '/users/login',
 	successRedirect: '/',
 	failureFlash: 'Invalid credentials' 
 }));
+
+router.get('/isauth', function (req, res) {
+    if (req.isAuthenticated()) {
+        res.status(200).jsonp({"isAuth": true, "message": "You're authenticated"});
+    } else {
+        res.status(401).jsonp({"isAuth": false,"message": "You're not authenticated"});
+    }
+});
 
 module.exports = router;
