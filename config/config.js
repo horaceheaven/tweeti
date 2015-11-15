118f8ea032eddf280c673a93ac1b69ed55f54d48
horaceheaven <hheaven@medullan.com>
2015-11-15 17:49:32 -0400

    Twitter callback url environment aware

--- config/config.js
+++ config/config.js
@@ -2,20 +2,26 @@ var config = {};
 
 config = {
     production: {
-
+        mongoURI: 'mongodb://localhost:27017/tweeti',
+        twitterCallBackUrl: 'http://tweeti.horaceheaven.com/auth/twitter/callback'
     },
     development: {
-        mongoURI: 'mongodb://localhost:27017/tweeti'
+        mongoURI: 'mongodb://localhost:27017/tweeti',
+        twitterCallBackUrl: 'http://127.0.0.1:3000/auth/twitter/callback'
     },
     test: {
-        mongoURI: 'mongodb://localhost:27017/tweeti'
+        mongoURI: 'mongodb://localhost:27017/tweeti',
+        twitterCallBackUrl: 'http://127.0.0.1:3000/auth/twitter/callback'
     },
     default: {
-        mongoURI: 'mongodb://localhost:27017/tweeti'
+        mongoURI: 'mongodb://localhost:27017/tweeti',
+        twitterCallBackUrl: 'http://127.0.0.1:3000/auth/twitter/callback'
     }
 };
 
 config.get = function(env) {
+    console.log('using configuration for', env);
+    console.log('configuration output', config[env]);
     return config[env] || config.default;
 };
 
